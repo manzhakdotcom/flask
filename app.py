b9f86645c63cac13f0b5267cc2525878ca6a7d60
Sergey Manzhak <manzhak.com@gmail.com>
2018-10-31 16:22:56 +0300

    Add title

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return render_template('index.html', content='Hello World!')
+    return render_template('index.html', content='Hello World!', the_title='Hello!')
 
 
 if __name__ == '__main__':
